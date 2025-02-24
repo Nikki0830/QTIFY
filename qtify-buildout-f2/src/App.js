18785f44ca43d5ab7684f22140669965cb810f31
Nikki0830 <gniki0830@gmail.com>
2025-02-24 13:19:39 +0530

    QTIFY_BUIDOUT

--- qtify-buildout-f2/src/App.js
+++ qtify-buildout-f2/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Hero from "./components/Hero/Hero";
-import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/Navbar/NavBar";
 import Section from "./components/Card/Section";
 
 function App() {
